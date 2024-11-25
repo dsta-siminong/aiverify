c36fc54845b928f100083d691782ad5ee856b9a8
simin-ong <ongsimin2000@gmail.com>
2024-11-25 11:20:07 +0800

    deleted files in job folders to move to another folder

--- setup-aiverify/aiverify-kube/docker/job/src/test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-print("hello world")
-
-var_one_val = os.environ.get('var_one', "not found")
-print(f"Value of VAR_ONE: {var_one_val}")
-
-var_two_val = os.environ.get('var_two', "not found")
-print(f"Value of VAR_TWO: {var_two_val}")
